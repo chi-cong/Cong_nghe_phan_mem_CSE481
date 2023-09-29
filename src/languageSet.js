72821e96318376700a19d32973dd010e77f8bcb2
Chi Cong <chicong422002@gmail.com>
2023-09-29 12:51:41 +0700

    adding dependencies

---
+++ src/languageSet.js
@@ -0,0 +1,52 @@
+const languageSet = {
+  vn: {
+    appName: "Danh sách việc",
+    filter: {
+      all: "Tất cả",
+      expired: "Hết hạn",
+      undone: "Cần làm",
+    },
+    message: {
+      expirationWarning: "Sắp đến hạn",
+      formAlert: "Hãy nhập đầy đủ thông tin",
+      maximumTaskCard: "Bạn chỉ có thể lưu tối đa 20 việc"
+    },
+    todoCard: {
+      task: "Việc: ",
+      deadline: "Hạn: ",
+    },
+    button: {
+      addCard: "Thêm thẻ mới",
+      save: "Lưu",
+      discard: "Hủy",
+      edit: "Sửa",
+      editCard: "Sửa thẻ"
+    },
+  },
+  en: {
+    appName: "Todo list",
+    filter: {
+      all: "All",
+      expired: "Expired",
+      undone: "Undone",
+    },
+    message: {
+      expirationWarning: "Expired soon",
+      formAlert: "Fill all inputs",
+      maximumTaskCard: "You can only have 20 task cards"
+    },
+    todoCard: {
+      task: "Task: ",
+      deadline: "Deadline: ",
+    },
+    button: {
+      addCard: "Add new card",
+      save: "Save",
+      discard: "Discard",
+      edit: "Edit",
+      editCard: "Edit Card"
+    },
+  },
+};
+
+export default languageSet;
